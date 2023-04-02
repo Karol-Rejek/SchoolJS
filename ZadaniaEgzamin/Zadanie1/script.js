abf3e9d841c2713695432502f3b4b37bf4c7bb0a
Karol Rejek <karolr.gaming02@gmail.com>
2023-04-02 18:18:50 +0200

    Zadanie1 Egzamin

---
+++ ZadaniaEgzamin/Zadanie1/script.js
@@ -0,0 +1,17 @@
+function pokazCene()
+{
+    var cena = 0;
+    if(document.getElementById('krotkie').checked) {
+    cena = 25;
+    }
+    if(document.getElementById('srednie').checked) {
+    cena = 30;
+    }
+    if(document.getElementById('poldlugie').checked) {
+    cena = 40;
+    }
+    if(document.getElementById('dlugie').checked) {
+    cena = 50;
+    }
+    document.getElementById('wynik').innerHTML = "Cena strzyżenia: " + cena + "zł";
+}
